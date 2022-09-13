d864d798857d9735e55b10168b6a958d5c0f5bed
Daniel <daniel.langton@iths.se>
2022-09-13 12:48:03 +0200

    Add project files.

---
+++ sebbes htmlgenerator/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Skriva ut data
+ */
+WebsiteGenerator website = new WebsiteGenerator();
+
+website.printPage();
+
+Console.WriteLine("------------------------------");
+
+WebsiteGenerator styledWebsite = new StyledWebsiteGenerator("RED");
+
+styledWebsite.printPage();
+
+
+class WebsiteGenerator
+{
+    /*
+     * Data ifrån API
+     */
+    string[] techniques = { "   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   " };
+    string[] messagesToClass = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
+
+    string className = "Klass A";
+    string kurser = "";
+
+
+    public virtual void printStart()
+    {
+        string start = "<!DOCTYPE html>\n<html>\n<body>\n<main>\n";
+        Console.WriteLine(start);
+    }
+    void printWelcome(string className, string[] message)
+    {
+        string welcome = $"<h1> Välkomna {className}! </h1>";
+
+        string welcomeMessage = "";
+
+        foreach (string msg in message)
+        {
+            welcomeMessage += $"\n<p><b> Meddelande: </b> {msg} </p>";
+        }
+
+        Console.WriteLine(welcome + welcomeMessage);
+    }
+    void printKurser()
+    {
+        string kurser = courseGenerator(this.techniques);
+        Console.WriteLine(kurser);
+    }
+    void printEnd()
+    {
+        string end = "</main>\n</body>\n</html>";
+        Console.WriteLine(end);
+    }
+
+    public void printPage()
+    {
+        printStart();
+        printWelcome(this.className, this.messagesToClass);
+        printKurser();
+        printEnd();
+    }
+    string courseGenerator(string[] techniques)
+    {
+
+        foreach (string technique in techniques)
+        {
+            string tmp = technique.Trim();
+            kurser += "<p>" + tmp[0].ToString().ToUpper() + tmp.Substring(1).ToLower() + "</p>\n";
+        }
+
+        return kurser;
+    }
+}
+
+class StyledWebsiteGenerator : WebsiteGenerator
+{
+    public string Color { get; set; }
+
+    public StyledWebsiteGenerator(string color)
+    {
+        Color = color;
+    }
+
+    public override void printStart()
+    {
+        string start = $"<!DOCTYPE html>\n<html>\n<head>\n<style>\np {{ color: {Color}; }}\n</style>\n</head>\n<body>\n<main>\n";
+        Console.WriteLine(start);
+    }    
+    
+}
